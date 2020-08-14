4cf97e3317252a996050e65c5b07d6f26d0860e5
Simon Diep <simondiep1@gmail.com>
2020-08-14 14:15:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # speech-to-text
+
+A simple web app that puts your speech into text.
+Leverages the [Chrome Web Speech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API).
+
+## Requirements
+
+Google Chrome Browser (not supported on iOS)
---
+++ script.js
@@ -0,0 +1,27 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+const recognition = new SpeechRecognition();
+recognition.continuous = false;
+recognition.lang = "en-US";
+
+const instructionsDiv = document.getElementById("instructions");
+const outputDiv = document.querySelector(".output");
+const bg = document.querySelector("html");
+
+document.onclick = function () {
+  recognition.start();
+  instructionsDiv.textContent = "Say something";
+};
+
+recognition.onresult = function (event) {
+  const word = event.results[0][0].transcript;
+  outputDiv.textContent = word;
+  bg.style.backgroundColor = word;
+};
+
+recognition.onspeechend = function () {
+  recognition.stop();
+  instructionsDiv.textContent = "Tap the screen";
+};
